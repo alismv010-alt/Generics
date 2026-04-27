b41319c9363719f734b2eed40f8f968b6db175b9
alismv010-alt <alismv010@gmail.com>
2026-04-27 23:38:26 +0400

    Lists

---
+++ src/Lists/ListsMain.java
@@ -0,0 +1,20 @@
+package Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListsMain {
+    public static void main (String[] args) {
+        /*
+        ArrayList<String> arrlist = new ArrayList<>();
+        arrlist.add("Vusale");
+        arrlist.add("Valeh");
+        arrlist.add("Ali");
+
+        for (String aa: arrlist) {
+            System.out.println(aa);
+        }
+
+         */
+    }
+}
---
+++ src/Lists/Myarrlist.java
@@ -0,0 +1,18 @@
+package Lists;
+
+import java.util.Arrays;
+
+public class Myarrlist <T> {
+    private Object[] array = new Object[100];
+    int i=0;
+    public void add (T pram) {
+        if (i==array.length) {
+            Object[] copyofarray = Arrays.copyOf(array, array.length/2);
+            this.array=copyofarray;
+        }
+    }
+    public T get (int a) {
+        return (T) array[i];
+    }
+
+}
